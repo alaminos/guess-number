9129f70950912ffdb947aff5742c8d895711a4f9
alaminos <aalaminosz@gmail.com>
2017-12-04 09:55:38 +0100

    commit
    wip

---
+++ guessNumber.js
@@ -0,0 +1,150 @@
+// things to fix:
+/*
+
+
+- if no number is entered, message: enter a number in the input field below
+- bigger input field
+-arreglar lo del salto de linea
+- print update tries left
+- fix line 82 and isNaN in line
+
+
+*/
+
+var guessTheNumber = {
+    
+    //initialize secret number
+    secretNumber: 0,
+
+    //initialize user's guess
+    userGuess: 0,
+
+    //counter for the number of tries
+    triesLeft: 4,
+
+    // getting a random number up to 1000 inclusive
+    getRandomNumber: function() {
+        this.secretNumber = Math.floor(Math.random() * 11);
+        //view.printInNumber(this.secretNumber); prints number in a box
+    },
+
+    // start game
+    startGame: function() {
+        this.getRandomNumber();
+    },
+
+    //is it right?
+    isRight: function(userInput) {
+        this.triesLeft -= 1;
+        view.clearInputField();
+        if (userInput === this.secretNumber) {
+            view.youWin();
+            view.isVisible("userInputForm", "none");
+            view.isVisible("startGameBtn", "block");
+        } else {
+            if (this.triesLeft > 0) { //if guess is wrong, first we check if the user ran out of tries
+                this.someFeedback(userInput); //if user is still alive, we offer her some feedback
+            } else {
+                view.gameOver(); //otherwise: gameOver
+                view.isVisible("userInputForm", "none");
+                view.isVisible("startGameBtn", "block");
+            };
+        }
+    },
+
+    //response to user
+    someFeedback: function(wrongGuess) {
+        if (isNaN(wrongGuess)) { //this evaluation should be removed from here 
+            //and be placed the first one in the isRight method, so that the
+            //call to view.guessesLog is immediately after it, way up in the process
+            // before it branch out in different scenarios
+            view.numbersOnly();
+        } else {
+            if (wrongGuess > this.secretNumber) {
+                view.wrongGuess("smaller than ", wrongGuess);
+            } else { 
+                view.wrongGuess("larger than ", wrongGuess);
+            }
+        }
+    },
+
+
+};
+
+
+var handlers = {
+    startGame: function() {
+        guessTheNumber.triesLeft = 4;
+        guessTheNumber.getRandomNumber();
+        view.welcome();
+        view.isVisible("userInputForm", "flex");
+        view.isVisible("startGameBtn", "none");
+    },
+
+    newGuess: function() {
+        var newGuess = document.getElementById("guessInput").valueAsNumber;
+        view.guessesLog(newGuess); //this call should go AFTER user input has been checked to be a number
+        guessTheNumber.isRight(newGuess);
+        return false;
+    }
+};
+
+
+var view = {
+
+    infoPanel: document.getElementById("infoPanel"),
+
+    welcome: function() {
+        //var welcomeMessage = document.getElementById("infoPanel");
+        this.infoPanel.textContent = "Hi, I have a number between 0 and 10 (both included). You have 4 tries to guess the number. ";
+        var triesPanel = document.getElementById("triesPanel");
+        triesPanel.textContent = "";
+    },
+
+    isVisible: function(element, display) {
+        var visibility = document.getElementById(element);
+        visibility.style.display = display;
+    },
+
+    wrongGuess: function(clue, wrongGuess) {
+        //var message = document.getElementById("infoPanel");
+        this.infoPanel.textContent = "Sorry, the secret number is " + clue + wrongGuess + "salto de línea" + " Please try again. "; //solucionar saltos de línea
+    },
+
+    numbersOnly: function() {
+        this.infoPanel.textContent = "You did not enter a number. Try again champion";
+    },
+
+    gameOver: function() {
+        //var gameOverMessage = document.getElementById("infoPanel");
+        this.infoPanel.textContent = "You ran out of tries. GAME OVER."
+    },
+
+    youWin: function() {
+        //var youwinMessage = document.getElementById("infoPanel");
+        this.infoPanel.textContent = "YOU WIN! Congratulations for such an absolutely useless waste of precious time.";
+    },
+
+    clearInputField: function() {
+        var guessInput = document.getElementById("guessInput");
+        guessInput.value = "";
+    },
+
+    guessesLog: function(newGuess) {
+        var triesPanel = document.getElementById("triesPanel");
+        triesPanel.textContent += " " + newGuess + " ";
+
+    }
+
+};
+
+
+
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+"Write a guessing game where the user has to guess a secret number. 
+After every guess the program tells the user whether their number was too large or too small. 
+At the end the number of tries needed should be printed.
+It counts only as one try if they input the same number multiple times consecutively."
+
+Having this program to work in the browser console was fairly quick.
+One of the first issues I encountered was when testing the first versions at isRight evaluation.
+if (userInput === secretNumber) was not going to work properly as the user input (coming from a prompt window) was a string, not a number.
+( So if I entered the right answer, the feedback would say "sorry the secret number is LARGER than"
+I guess that 45 -number- is larger than "45". )
+It was fixed using == instead of ===. It could have probably been fixed converting the string into a number value.
+Later when I had a user interface, it was fixed fetching the user number with the method .valueAsNumber, and got the triple = back into the isRight evaluation.
+
+Although the challenge was to write a program that simply worked, most of the time is employed on css, especially dealing with media queries. I very much prefer writing the logic of the program, than googling how to style pretty buttons. 
